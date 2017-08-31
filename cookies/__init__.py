266441cf679b396e8561fecbc62764be23f93b44
Chang <HeawerCher@gmail.com>
2017-08-31 11:43:45 +0800

    更改Cookies类的cookie存储逻辑

--- cookies/__init__.py
+++ cookies/__init__.py
@@ -6,12 +6,8 @@ __dir__ = os.path.dirname(__file__)
 class Cookies:
     def __init__(self, domain):
         self.domain = domain
+        self._cookies = {}
         self.path = f"{os.path.join(__dir__, self.domain)}.txt"
-
-    def __bool__(self):
-        return os.path.exists(self.path)
-
-    def __iter__(self):
         if self:
             try:
                 with open(self.path) as f:
@@ -20,4 +16,13 @@ class Cookies:
                 raise NotImplementedError('cookies 格式不支持')
             else:
                 for c in c:
-                    yield c['name'], c['value']
+                    self._cookies[c['name']] = c['value']
+        else:  # 文件不存在便尝试从浏览器中读取
+            pass
+
+    def __bool__(self):
+        return os.path.exists(self.path)
+
+    def __iter__(self):
+        for k, v in {}.items():
+            yield k, v
